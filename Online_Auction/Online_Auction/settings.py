d47f7891889c6f59a6040fd780c9c888f140f0ca
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2024-11-28 22:48:54 +0300

    added models and and registered on admin.py

--- Online_Auction/Online_Auction/settings.py
+++ Online_Auction/Online_Auction/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Online_Auction.urls'
+ROOT_URLCONF = 'Online_Auction.urls',
 
 TEMPLATES = [
     {
@@ -116,9 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField',
--- Online_Auction/auction/models.py
+++ Online_Auction/auction/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Bidder(models.model):
+    name = models.CharField(max_length=60),
+
+
+class Result(models.model):
+    name = models.CharField(max_length=60),
+
+class Payment(models.model):
+    name = models.CharField(max_length=60),
+
+class Member_fee(models.model):
+    name = models.CharField(max_length=60),
+
+class Status(models.model):
+    name = models.CharField(max_length=60),
+
+class Send_Feedback(models.model):
+    name = models.CharField(max_length=60),
+
+class Auction_User(models.model):
+    name = models.CharField(max_length=60),
+
+class Category(models.model):
+    name = models.CharField(max_length=60),
+
+class Sub_Category(models.model):
+    name = models.CharField(max_length=60),
+
+class Session_date(models.model):
+    name = models.CharField(max_length=60),
+
+class Session_Time(models.model):
+    name = models.CharField(max_length=60),
+
+class Product(models.model):
+    name = models.CharField(max_length=60),
+
+class Aucted_Product(models.model):
+    name = models.CharField(max_length=60),
+
+class Participant(models.model):
+    name = models.CharField(max_length=60),
+
+
+
